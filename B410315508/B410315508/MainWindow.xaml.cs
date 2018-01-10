7a7b43321b1cc436b70f55cf00941b336353439f
s1031550 <kblbj32417@yahoo.com.tw>
2018-01-10 18:27:13 +0800

    final exam

---
+++ B410315508/B410315508/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace B410315508
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            menuwin.mydraw = this.ink1;
+            menuwin.Show();
+        }
+        Window1 menuwin = new Window1();
+    
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            DirectoryInfo dirinfo = new DirectoryInfo(@"C:\Users\Public\Pictures\Sample Pictures ");
+            FileInfo[] files = dirinfo.GetFiles();
+            foreach (FileInfo fi in files)
+            {
+                if (fi.FullName.EndsWith(".jpg"))
+                {
+                    Image img = new Image();
+                    BitmapSource src = new BitmapImage(new Uri(fi.FullName));
+                    img.Source = src;
+                    img.Width = 80;
+                    img.Margin = new Thickness(10);
+                    this.wrapPanel1.Children.Add(img);
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ B410315508/B410315508/Window1.xaml.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Ink;
+
+namespace B410315508
+{
+    /// <summary>
+    /// Window1.xaml 的互動邏輯
+    /// </summary>
+    public partial class Window1 : Window
+    {
+        public Window1()
+        {
+            InitializeComponent();
+        }
+        public InkCanvas mydraw;
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            mydraw.EditingMode = InkCanvasEditingMode.Ink;
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Red;
+            da.Width = 20;
+            mydraw.DefaultDrawingAttributes = da;
+
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            mydraw.EditingMode = InkCanvasEditingMode.Ink;
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Red;
+            da.Width = 5;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button3_Click(object sender, RoutedEventArgs e)
+        {
+            mydraw.EditingMode = InkCanvasEditingMode.Ink;
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Green;
+            da.Width = 20;
+            mydraw.DefaultDrawingAttributes = da;
+
+        }
+
+        private void button4_Click(object sender, RoutedEventArgs e)
+        {
+            mydraw.EditingMode = InkCanvasEditingMode.Ink;
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Green;
+            da.Width = 5;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button5_Click(object sender, RoutedEventArgs e)
+        {
+            mydraw.EditingMode = InkCanvasEditingMode.EraseByPoint;
+        
+
+        }
+        private void button6_Click(object sender, RoutedEventArgs e)
+        {
+            mydraw.EditingMode = InkCanvasEditingMode.EraseByStroke;
+        }
+
+        private void button7_Click(object sender, RoutedEventArgs e)
+        {
+            mydraw.EditingMode = InkCanvasEditingMode.Select;
+        }
+
+        private void button8_Click(object sender, RoutedEventArgs e)
+        {
+            mydraw.EditingMode = InkCanvasEditingMode.Ink;
+            DrawingAttributes da = new DrawingAttributes();
+            da.Color = Colors.Black;
+            da.Width = 10;
+            mydraw.DefaultDrawingAttributes = da;
+        }
+
+        private void button9_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+        
+
+        private void button10_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
